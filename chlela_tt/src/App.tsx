c6e701b7b8f79a6a1449244b6daf24d66d375ae5
vitaly-redkin <v21267@gmail.com>
2018-07-18 11:45:38 +0300

    TSLint tslint:latest and tslint-microsoft-contrib rule sets applied
    tslint:latest and tslint-microsoft-contrib rule sets applied.
    
    Exceptions are set to:
    
    "no-relative-imports": false,
    "no-import-side-effect": [true, {"ignore-module": "(\\.css)$"}]
    
    The first rule allow relative imports. It seems MS itself is going to deprecate them (https://github.com/Microsoft/tslint-microsoft-contrib/issues/435).
    
    The second rule allows CSS files to be imported without an "import with side effects" warning.

--- chlela_tt/src/App.tsx
+++ chlela_tt/src/App.tsx
@@ -1,14 +1,16 @@
-import * as React from "react";
-import * as styles from "./App.css";
+/**
+ * The application root component.
+ */
 
-const logo = require("./logo.svg");
+import * as React from 'react';
+import * as styles from './App.css';
 
-class App extends React.Component<{}, {}> {
-  public render() {
+export class App extends React.Component<{}, {}> {
+  public render() : JSX.Element {
     return (
       <div className={styles.app}>
         <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
+          <img src={'./logo.svg'} className={styles.appLogo} alt='logo' />
           <h2>Welcome to React</h2>
         </div>
         <p className={styles.appIntro}>
@@ -18,5 +20,3 @@ class App extends React.Component<{}, {}> {
     );
   }
 }
-
-export default App;
